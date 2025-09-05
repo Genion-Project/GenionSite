59cc63e9239cf3f35153fd047d3db1e28686cc2d
nafilhabibi <nafilhabibi07@gmail.com>
2025-09-06 01:24:42 +0700

    Genion Site Beta

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        float: 'float 4s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+};
